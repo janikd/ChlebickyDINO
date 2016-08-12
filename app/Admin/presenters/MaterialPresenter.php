e0a4ba4cdf8e56ff5da41795e86ccb6de723866c
koty <jan.kotrba@bitbucket.com>
2016-08-12 23:02:47 +0200

    Material View

--- app/Admin/presenters/MaterialPresenter.php
+++ app/Admin/presenters/MaterialPresenter.php
@@ -5,10 +5,7 @@
 
 namespace App\Admin\Presenters;
 
-use App\Admin\Components\CreateResourceCategoryFormFactory;
-use App\Admin\Model\NotFoundException;
 use App\Admin\Model\Resources\CategoryManager;
-use App\Admin\Model\UpdateModelException;
 
 /**
  * Class MaterialPresenter
@@ -16,67 +13,9 @@ use App\Admin\Model\UpdateModelException;
  */
 class MaterialPresenter extends SecuredPresenter
 {
-	/**
-	 * @var CategoryManager
-	 */
-	private $categoryManager;
 
-	/** @var CreateResourceCategoryFormFactory @inject */
-	public $createResourceCategoryFormFactory;
-
-	/**
-	 * MaterialPresenter constructor.
-	 * @param CategoryManager $categoryManager
-	 */
-	public function __construct(CategoryManager $categoryManager)
+	public function renderDefault($id)
 	{
-		parent::__construct();
-		$this->categoryManager = $categoryManager;
-	}
-
-	/**
-	 * @param $categoryId
-	 */
-	public function handleMoveUp($categoryId)
-	{
-		try {
-			$this->categoryManager->moveUp($categoryId);
-			$this->redirect('this');
-		} catch (NotFoundException $e) {
-			$this->flashMessage('Kategorie neexistuje.', 'danger');
-		} catch (UpdateModelException $e) {
-			$this->flashMessage('Přesunutí na vyšší pozici selhalo.', 'danger');
-		}
-	}
-
-	/**
-	 * @param $categoryId
-	 */
-	public function handleMoveDown($categoryId)
-	{
-		try {
-			$this->categoryManager->moveDown($categoryId);
-			$this->redirect('this');
-		} catch (NotFoundException $e) {
-			$this->flashMessage('Kategorie neexistuje.', 'danger');
-		} catch (UpdateModelException $e) {
-			$this->flashMessage('Přesunutí na nižší pozici selhalo.', 'danger');
-		}
-	}
-
-	/**
-	 *
-	 */
-	public function renderCategory()
-	{
-		$this->getTemplate()->categories = $this->categoryManager->findAll();
-	}
-
-	/**
-	 * @return \Nette\Application\UI\Form
-	 */
-	public function createComponentCreateResourceCategoryForm()
-	{
-		return $this->createResourceCategoryFormFactory->create();
+		$this->getTemplate()->category = $this->getCategoryManager()->find($id, CategoryManager::THROW_EXCEPTION);
 	}
 }
