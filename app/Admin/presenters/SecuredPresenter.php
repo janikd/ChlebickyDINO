eb094de21688bf9e27eef401812cd8813908a8bf
koty <jan.kotrba@bitbucket.com>
2016-08-12 23:04:00 +0200

    SecuredPresenter @doc, materialCategories

--- app/Admin/presenters/SecuredPresenter.php
+++ app/Admin/presenters/SecuredPresenter.php
@@ -8,24 +8,39 @@ namespace App\Admin\Presenters;
 use App\Admin\Model\Resources\CategoryManager;
 use Nette\Application\UI\Presenter;
 
+/**
+ * Class SecuredPresenter
+ * @package App\Admin\Presenters
+ */
 class SecuredPresenter extends Presenter
 {
-	protected $materialCategories;
-
+	/**
+	 * @var CategoryManager
+	 */
 	private $categoryManager;
 
+	/**
+	 * SecuredPresenter constructor.
+	 * @param CategoryManager $categoryManager
+	 */
 	public function __construct(CategoryManager $categoryManager)
 	{
 		parent::__construct();
 		$this->categoryManager = $categoryManager;
 	}
 
+	/**
+	 * Put materialCategories into template variables before render
+	 */
 	public function beforeRender()
 	{
 		parent::beforeRender();
-		$this->materialCategories = $this->categoryManager->findAll();
+		$this->getTemplate()->materialCategories = $this->categoryManager->findAll();
 	}
 
+	/**
+	 * Presenter startup lifehook
+	 */
 	public function startup()
 	{
 		parent::startup();
@@ -35,10 +50,21 @@ class SecuredPresenter extends Presenter
 		}
 	}
 
+	/**
+	 * Handle logout action
+	 */
 	public function handleLogOut()
 	{
 		$this->getUser()->logout(TRUE);
 		$this->flashMessage('Váš účet byl odhlášen.', "success");
 		$this->redirect('Auth:signIn');
+	}
+
+	/**
+	 * @return CategoryManager
+	 */
+	public function getCategoryManager()
+	{
+		return $this->categoryManager;
 	}
 }
